94174882f38d510316bc4128d976b3ef08f24286
sakshikachare <sakshikachare786@gmail.com>
2024-09-08 22:27:58 +0530

    minimum requirements for funding

--- sample.java
+++ sample.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class sample {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int HeOnMoon = x*y;
+		    int Energy = a*b;
+		    if(HeOnMoon>= Energy) {
+		        System.out.println("Yes");
+		    }
+		    else {
+		        System.out.println("No");
+		    }
+		
+		    
+		}
 
+	}
+	
 }
