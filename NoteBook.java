7ab4908d9db294c42eba3143b8ddaef2c1b7afb0
sakshikachare <sakshikachare786@gmail.com>
2024-09-09 12:31:03 +0530

    weigth of the pulp factory in kg

---
+++ NoteBook.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NoteBook {
+
+	public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+			int t = scanner.nextInt();
+			while(t-->0) {
+				int n = scanner.nextInt();
+				System.out.println(n*10);
+			}
+
+	}
+
+}
---
+++ SugarCane.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SugarCane {
+
+	public static void main(String[] args) {
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			int t = sc.nextInt();
+			while(t-->0) {
+				int n = sc.nextInt();
+				System.out.println(n*15);
+			}
+		}
+		
+		
+
+	}
+
+}
